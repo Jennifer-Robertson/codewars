fd3436141277dc6303e9d3dec5d4d3147f458bc0
robertsonj48 <robertsonj48@gmail.com>
2022-10-04 20:51:42 -0400

    104

---
+++ 10-4.js
@@ -0,0 +1,21 @@
+// Given a string of words (x), you need to return an array of the words, sorted alphabetically by the final character in each.
+
+// If two words have the same last letter, they returned array should show them in the order they appeared in the given string.
+
+// All inputs will be valid.
+
+function last(x){
+    return (x.split(" ").sort((a,b) => {
+      if (a[a.length-1] < b[b.length-1]) {
+        return -1;
+      }
+      if (a[a.length-1] > b[b.length-1]) {
+        return 1;
+      }
+      return 0;
+    }))
+      
+      
+    }
+                      
+    
